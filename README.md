e5c9a30029453b84548d970b3a4a23cb09481924
Matias Lahore <matiaslahore@gmail.com>
2020-04-16 08:32:36 -0300

    push to git

---
+++ README.md
@@ -0,0 +1,19 @@
+#GrapQL API in Node.js
+
+Its a Base Node.js API with GraphQL
+
+### Instructions
+
+Clone Proyect
+```
+git clone https://github.com/matiaslahore/graphql-node.git
+```
+cd and install
+```
+cd graphql-node && npm i
+```
+
+Add your MongDB into App.js and Run proyect
+```
+npm start
+```
---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// connect to mongo db database (mlab)
+mongoose.connect('');
+mongoose.connection.once('open', () => {
+    console.log('conneted to database');
+});
+
+// bind express with graphql
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+app.listen(3000, () => {
+    console.log('now listening for requests on port 3000');
+});
